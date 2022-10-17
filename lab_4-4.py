b4103f22d6c8fe4a7a907bca506380f353d8f162
Jan <p23jsalafia@fairfieldprep.org>
2022-10-17 13:34:09 -0400

    4-4 Submission 4-5 Submission

---
+++ lab_4-4.py
@@ -0,0 +1,20 @@
+#Creator JS 10/14/2022
+from gettext import find
+from operator import index
+
+#Find the first occurance of t 
+word ="flibbertigibbet"
+print(word.find('t'))
+
+#Assigning that index value as a variable
+var_1 = word.find("t")
+print(var_1)
+
+#Print first name in all uppercase letters
+first_name = 'jan'
+print(first_name.upper())
+
+#Print sentence with splits at each comma
+ss = "I wish, I wish, I was a fish."
+print(ss.split(","))
+
---
+++ lab_4-5.py
@@ -0,0 +1,10 @@
+#Creator JS 10/13/2022
+#Input Person 1
+print("Please enter person one's name:")
+person1 = input()
+#Input Person 2
+print("Please etnter person two's name:")
+person2 = input()
+#Print the sentence using interpoplation
+fs = "Hello, {0}. My name is {1}.".format(person1, person2)
+print(fs)
